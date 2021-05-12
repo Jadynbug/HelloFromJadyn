d52afee2ba6968363f517d6d11ce7e552ab95613
Jadyn Iverson <bookgirldreams88@yahoo.com>
2021-05-12 10:20:50 -0600

    Fixed Case's line of code

--- src/Iverson/Main.java
+++ src/Iverson/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jadyn");
-        System.out.println("Hello, Jonathan. My name is Jayden.");
+        System.out.println("Hello, Jadyn. My name is Case.");
     }
 }
